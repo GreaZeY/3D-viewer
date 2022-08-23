47432405a8344b8932e98d7a219a49511a24fb50
vishal1145 <vishal.kumar1145@gmail.com>
2022-08-23 19:19:51 +0530

    bum map export

--- src/Components/Pendant.jsx
+++ src/Components/Pendant.jsx
@@ -1,19 +1,44 @@
-import { Box } from "@react-three/drei";
+import { Box, Sphere, Html } from "@react-three/drei";
 import * as THREE from "three";
+import { exportWithMap } from "../utils/exporters";
+import { useRef } from "react";
+
+import { computeUV } from "./utils";
+let bmap;
 const Pendant = ({ bumpMap }) => {
-  console.log(bumpMap);
+  const box = useRef();
+
   const drawBump = (e) => {
     if (bumpMap) {
-      let bmap = THREE.ImageUtils.loadTexture(URL.createObjectURL(bumpMap));
+      bmap = new THREE.TextureLoader().load(URL.createObjectURL(bumpMap));
+
       e.bumpMap = bmap;
-      e.bumpScale = -0.15;
-      e.needsUpdate = true;
+      e.bumpScale = -0.1;
+      e.displacementMap = bmap;
+      e.displacementScale = -0.1;
+
+      e.displacementBias = 0.0;
+      e.bumpScale = .5;
+      e.needsUpdate= true
+
     }
   };
+
+  if(box.current){
+    computeUV(box.current);
+  }
+
+ 
+
   return (
     <>
-      <Box args={[100, 100, 1]}>
+      <Box
+        ref={box}
+        onUpdate={(e) => console.log(e.geometry)}
+        args={[100, 100, 1]}
+      >
         <meshStandardMaterial
+        skinning
           onUpdate={drawBump}
           attach="material"
           color="rgb(255, 201, 0)"
@@ -21,6 +46,21 @@ const Pendant = ({ bumpMap }) => {
           roughness={0.2}
         />
       </Box>
+      {/* <mesh ref={box}>
+        <torusGeometry args={[20, 6, 32, 100]} />
+        <meshStandardMaterial
+          onUpdate={drawBump}
+          attach="material"
+          color="rgb(255, 201, 0)"
+          metalness={1}
+          roughness={0.4}
+        />
+      </mesh> */}
+      <Html position={[50, 50, 0]}>
+        <button onClick={() => exportWithMap(box.current, bumpMap, bmap)}>
+          export
+        </button>
+      </Html>
     </>
   );
 };
