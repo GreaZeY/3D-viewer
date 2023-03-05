2d09dc5971ec4d6a521436553d4637b8876e0d18
greazey <greaserevive@gmail.com>
2023-03-05 18:39:31 +0530

    click away listener improved

--- src/Components/Model.jsx
+++ src/Components/Model.jsx
@@ -1,22 +1,15 @@
 import D3text from "./Objects/D3text";
 import { forwardRef, useCallback, useEffect, useRef, useState } from "react";
-import { useThree, useFrame } from "@react-three/fiber";
 import Controls from "@/Tools/Controls";
 import useUpdateControlValues from "@/Hooks/useUpdateControlValues";
 import { materialProps } from "lib/constants/defaulProps";
 import Objects from "./Objects/Objects";
-
-let clickAway = false;
 
 const Model = forwardRef(({ props }, ref) => {
   const { textProps, guiControls } = props;
   const [selectedObject, setSelectedObject] = useState(null);
 
   const transform = useRef();
-
-  const {
-    gl: { domElement },
-  } = useThree();
 
   // hide transform and gui controls
   const closeControls = useCallback(() => {
@@ -27,24 +20,6 @@ const Model = forwardRef(({ props }, ref) => {
   const { color, metalness, roughness } = useUpdateControlValues(closeControls);
 
   useEffect(() => {
-    closeControls();
-    // listeners
-
-    // click away listener for transform controls
-    const canvasClickListener = () => {
-      if (clickAway) {
-        closeControls();
-        clickAway = !clickAway;
-      }
-    };
-    domElement.addEventListener("click", canvasClickListener);
-    // cleanup for listeners
-    return () => {
-      domElement.removeEventListener("click", canvasClickListener);
-    };
-  }, [closeControls, domElement]);
-
-  useEffect(() => {
     if (!selectedObject) return;
     let mat = selectedObject.material;
     mat.color.set(color);
@@ -52,25 +27,13 @@ const Model = forwardRef(({ props }, ref) => {
     mat.metalness = metalness;
   }, [color, metalness, roughness, selectedObject]);
 
-  // three render loop
-  useFrame((state) => {
-    // click away listener for transform controls
-    let children = [ref.current];
-    transform.current?.children[0].traverse((kid) => {
-      if (kid.type === "Mesh") children.push(kid);
-    });
-
-    let intersectsTrans = state.raycaster.intersectObjects(children);
-    if (intersectsTrans.length > 0) {
-      clickAway = false;
-    } else {
-      clickAway = true;
-    }
-  });
-
   return (
     <>
-      <object3D ref={ref} onClick={(e) => setSelectedObject(e.object)}>
+      <object3D
+        ref={ref}
+        onClick={(e) => setSelectedObject(e.object)}
+        onPointerMissed={(e) => closeControls()}
+      >
         <D3text {...textProps} {...materialProps} />
         <Objects />
         {/* <Box bumpMap={texture}/> */}
